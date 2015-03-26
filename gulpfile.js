26a4525b6efe6becfdc80efd31b288717448f2ee
Tom Durkin <tom.durkin1@gmail.com>
2015-03-26 10:21:51 +0000

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,8 @@
-// Building the mothership
+// building the mothership
+
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),    
+    sass = require('gulp-sass'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    browserSync = require('browser-sync');  
